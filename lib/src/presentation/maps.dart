75312740a538c7d062b3b0010b74508f3d340cb8
Steve Joels <63671647+stevejoels54@users.noreply.github.com>
2022-10-14 21:11:33 +0300

    Added google maps

---
+++ lib/src/presentation/maps.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:async';
+
+class MapScreen extends StatefulWidget {
+  final String placename;
+  final double latitude;
+  final double longitude;
+  const MapScreen({
+    Key? key,
+    required this.latitude,
+    required this.longitude,
+    required this.placename,
+  }) : super(key: key);
+
+  @override
+  State<MapScreen> createState() => _MapState();
+}
+
+class _MapState extends State<MapScreen> {
+  final Completer<GoogleMapController> _controller = Completer();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Maps'),
+        backgroundColor: Colors.black,
+        elevation: 0.0,
+      ),
+      body: GoogleMap(
+        mapType: MapType.normal,
+        markers: {
+          Marker(
+            markerId: MarkerId(widget.placename),
+            position: LatLng(widget.latitude, widget.longitude),
+          ),
+        },
+        initialCameraPosition: CameraPosition(
+          target: LatLng(widget.latitude, widget.longitude),
+          zoom: 20.0,
+        ),
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+    );
+  }
+}
